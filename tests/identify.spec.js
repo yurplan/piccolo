886ffa739e226d6275faa3efb9cd4792472bdd56
Caligone <pierrejean.leger.pro@gmail.com>
2017-10-03 09:45:56 +0200

    Add tests on identify module

--- tests/identify.spec.js
+++ tests/identify.spec.js
@@ -1,4 +1,4 @@
-
+const rewire = require('rewire');
 const Lab = require('lab');
 
 const lab = Lab.script();
@@ -7,12 +7,50 @@ const describe = lab.describe;
 const it = lab.it;
 const expect = Lab.assertions.expect;
 
-const identify = require('../lib/identify');
+const identify = rewire('../lib/identify');
 
 describe('Identification', () => {
   it('returns error if no file', (done) => {
     identify({}).catch((err) => {
       expect(err.message.indexOf('Request did not return an image.') > -1).to.equal(true);
+      done();
+    });
+  });
+  it('returns error if invalid format', (done) => {
+    // eslint-disable-next-line no-underscore-dangle
+    identify.__set__('gm', () => ({
+      identify: (cb) => {
+        cb(false, {
+          format: 'GIF',
+        });
+      },
+    }));
+    identify({}).catch((err) => {
+      expect(err).to.equal('INVALID_FORMAT');
+      done();
+    });
+  });
+  it('returns error if failed to start gm', (done) => {
+    // eslint-disable-next-line no-underscore-dangle
+    identify.__set__('gm', () => {
+      throw new Error('Testing');
+    });
+    identify({}).catch((err) => {
+      expect(err.message).to.equal('Testing');
+      done();
+    });
+  });
+  it('returns no error if success', (done) => {
+    // eslint-disable-next-line no-underscore-dangle
+    identify.__set__('gm', () => ({
+      identify: (cb) => {
+        cb(false, {
+          format: 'PNG',
+        });
+      },
+    }));
+    identify({}).then((info) => {
+      expect(info.format).to.equal('PNG');
       done();
     });
   });
